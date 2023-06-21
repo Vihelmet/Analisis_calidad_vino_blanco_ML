69d2b7ef5973751110215016e48e598723f41e9f
Vihelmet <129219070+Vihelmet@users.noreply.github.com>
2023-06-21 21:19:04 +0200

    June 21

--- src/data_processing.py
+++ src/data_processing.py
@@ -9,3 +9,12 @@ vino_features = vino.drop(columns='quality')
 vino_features
 
 vino_target = vino['quality']
+
+def csv(nombre_archivo, archivo_guardar):
+
+
+    ruta_archivo = '../data/' + nombre_archivo + '.csv'
+    archivo_guardar.to_csv(ruta_archivo, index=False)
+
+#csv("test", vino_features)
+#csv("train", vino)
