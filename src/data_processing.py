2397cd41b78a84d08143bb02f010db62f297aa88
Vihelmet <129219070+Vihelmet@users.noreply.github.com>
2023-06-23 18:49:47 +0200

    June 23

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,28 +1,17 @@
-from variables import *
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import os
+
+# Cargar raw
+
+vino = pd.read_csv(r'.\Analisis_calidad_vino_blanco_ML\data\raw\winequality-white.csv', delimiter= ';')
+
+# Procesar features
+
+# Guardar processed
+
+vino.to_csv(r'.\Analisis_calidad_vino_blanco_ML\data\processed.csv')
+vino.to_csv(r'.\Analisis_calidad_vino_blanco_ML\data\train.csv')
+vino.to_csv(r'.\Analisis_calidad_vino_blanco_ML\data\test.csv')
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-print(parent_dir)
 
-
-vino = pd.read_csv(parent_dir + '/data/raw/winequality-white.csv', delimiter= ';')
-vino
-
-vino_features = vino.drop(columns=['fixed acidity', 'citric acid', 'residual sugar', 'free sulfur dioxide', 'total sulfur dioxide', 'pH', 'sulphates','quality'])
-vino_features
-
-vino_target = vino['quality']
-
-# def csv(nombre_archivo, archivo_guardar):
-
-
-#     ruta_archivo = './data/' + nombre_archivo + '.csv'
-#     archivo_guardar.to_csv(ruta_archivo, index=False)
-
-# csv("test", vino)
-# csv("train", vino)
