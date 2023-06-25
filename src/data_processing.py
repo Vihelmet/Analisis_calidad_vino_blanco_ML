1ea606d391fbc80a364054242a9648150893b442
Vihelmet <129219070+Vihelmet@users.noreply.github.com>
2023-06-25 18:16:37 +0200

    June 25

--- src/data_processing.py
+++ src/data_processing.py
@@ -4,10 +4,9 @@ import numpy as np
 vino = pd.read_csv(r'.\Analisis_calidad_vino_blanco_ML\data\raw\winequality-white.csv', delimiter= ';')
 
 vino['good quality'] = [1 if x > 5 else 0 for x in vino.quality]
+vino.drop(columns=['quality'], inplace=True)
 
 vino.to_csv(r'.\Analisis_calidad_vino_blanco_ML\data\processed.csv')
-vino.to_csv(r'.\Analisis_calidad_vino_blanco_ML\data\train.csv')
-vino.to_csv(r'.\Analisis_calidad_vino_blanco_ML\data\test.csv')
 
 
 
